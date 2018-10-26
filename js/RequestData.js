7fa791fa83b2ad3714c1ef041ce6d0921b84451b
LeTrongThang <niken2202@gmail.com>
2018-10-27 00:33:29 +0700

    Add inject facebook

---
+++ js/RequestData.js
@@ -0,0 +1,65 @@
+// Initialize Firebase
+
+var config = {
+  apiKey: "AIzaSyDHc28T74L7bhKdS3tI_uiC9SvvgpAZub0",
+  authDomain: "ilovekanji-fa097.firebaseapp.com",
+  databaseURL: "https://ilovekanji-fa097.firebaseio.com",
+  projectId: "ilovekanji-fa097",
+  storageBucket: "ilovekanji-fa097.appspot.com",
+  messagingSenderId: "565821302386"
+};
+firebase.initializeApp(config);
+var dataWord = "";
+var index = 0;
+var Kanji = document.getElementById("text");
+var Mean = document.getElementsByClassName("mm")[0];
+var On = document.getElementsByClassName("on")[0];
+var Kun = document.getElementsByClassName("kun")[0];
+var example = document.getElementsByClassName("example")[0];
+      
+
+loadWord("");
+function loadWord(type) {
+  if (type == 'up') {
+      index++;
+  } else {
+      if (index > 0)
+          index--;
+  }
+  var wordRef = firebase.database().ref().child(index);
+  wordRef.once('value', function (data) {
+      Kanji.innerText = data.val().Kanji;
+      Mean.innerText = data.val().Mean;
+      On.innerText = "On: "+data.val().On;
+      Kun.innerText = "Kun: "+data.val().Kun.replace( /\s\s+/g,',');
+      example.innerText = data.val().Example;
+  })
+ 
+}
+
+/*Add listener */
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode == 39) {
+      loadWord("up");
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode ==32 ) {
+      loadWord("up");
+  }
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode == 37) {
+      loadWord("down");
+  }
+});
+
+var element = document.getElementsByClassName("control-right")[0];
+element.addEventListener("click", function () {
+  loadWord("up");
+});
+
+document.getElementsByClassName("control-left")[0].addEventListener("click", function () {
+  loadWord("down");
+});
---
+++ js/injectfb.js
@@ -0,0 +1,24 @@
+
+window.onload = function () {
+    //get Data
+    var url = "https://cdn.jsdelivr.net/gh/niken2202/niken2202.github.io@d6de876d3a755cc76b4710024f70340c03c6cfb0/kanjilib.json";
+    jQuery.getJSON(url, function (json) {
+        var index = Math.floor(Math.random() * (json.length - 1)) + 0;
+        var word = json[index];
+        var container = document.getElementsByClassName("_4-u2 _1-ib _2tyk _20os _4-u8");
+        var html = "";
+        word.Example = word.Example.replace( /\n|\r\n/g, "<br>");
+        html += '<div class="wrapper">'
+            + '<div class="kanji">' + word.Kanji + '</div>'
+            + '<div class="mean">' + word.Mean + '</div>'
+            + '<div class="on">On: ' + word.On + '</div>'
+            + '<div class="kun">kun: ' + word.Kun + '</div>'
+            + '<div class="example">' + word.Example + '</div>'
+            + '</div>'
+            + '<div class="sign">Created with <font color="red">❤</font> by Le Trong Thang </div>';
+        container[0].innerHTML = html;
+        container[2].innerHTML = html;
+    });
+}
+
+
